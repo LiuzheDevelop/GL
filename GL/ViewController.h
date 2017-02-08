280309c4f71df9a3043214d78e751048ad8f62b6
刘喆 <liuzhe@liuzhedemacbook-pro.local>
2017-02-08 16:55:24 +0800

    Initial Commit

---
+++ GL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GL
+//
+//  Created by 刘喆 on 17/2/8.
+//  Copyright © 2017年 ll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GL
+//
+//  Created by 刘喆 on 17/2/8.
+//  Copyright © 2017年 ll. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
